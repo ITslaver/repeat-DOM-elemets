6ab1f6bfa67ea7587d33bb47f52a3b70b3a3c6eb
Viacheslav Polishchuk <mr_v.p@ukr.net>
2022-10-01 20:27:34 +0300

    first commit

---
+++ js/repeatDom.js
@@ -0,0 +1,52 @@
+// ------------------------------------------1-------------------------------------
+
+// ------------------------------------------2-------------------------------------
+// function buttonClick() {
+//   let input = document.getElementById("input");
+//   input.value = "!!!";
+// }
+// ------------------------------------------3-----------------------------------
+// function buttonClick() {
+//   let input = document.getElementById("input");
+//   alert(input.value);
+// }
+// -----------------------------------------4-----------------------------------
+// function buttonClick() {
+//   let input = document.getElementById("input");
+//   let number = Number(input.value);
+//   let square = number * number;
+//   alert(square);
+// }
+// -----------------------------------------5-----------------------------------
+// function buttonClick() {
+//   let input1 = document.getElementById("input1");
+//   let input2 = document.getElementById("input2");
+//   let input1Value = input1.value;
+//   let input2Value = input2.value;
+//   input1.value = input2Value;
+//   input2.value = input1Value;
+// }
+// -----------------------------------------6-----------------------------------
+// function buttonClick(elem) {
+//   elem.value = "Новый текст кнопки";
+// }
+// -----------------------------------------7-----------------------------------
+// function buttonClick() {
+//   document.getElementById("input").style.color = "red";
+// }
+// -----------------------------------------8-----------------------------------
+//Заблокирует элемент
+// function button1Click() {
+//   let input = document.getElementById("input");
+//   input.disabled = true;
+// }
+
+//Разблокирует элемент
+// function button2Click() {
+//   let input = document.getElementById("input");
+//   input.disabled = false;
+// }
+
+// ----------------------------------Задачи для решения-----------------------------
+
+// -----------------------------------------1---------------------------------------
